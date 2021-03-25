0c789198c80e06de57354308cce8182ecb515077
jjjjonathan <hornathanjon@gmail.com>
2021-03-24 22:46:31 -0500

    working tests for caesar

--- caesar.js
+++ caesar.js
@@ -1,5 +1,16 @@
 function caesar(input) {
-  return input;
+  const caesarArray = input.split("").map((char) => {
+    const code = char.charCodeAt(0);
+    if (code === 32) {
+      return char;
+    } else if ((code >= 97 && code <= 109) || (code >= 65 && code <= 77)) {
+      return String.fromCharCode(code + 13);
+    } else {
+      return String.fromCharCode(code - 13);
+    }
+  });
+
+  return caesarArray.join("");
 }
 
 module.exports = caesar;
